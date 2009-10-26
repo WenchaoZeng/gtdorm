9ef6850dd33c7c39aecd0223d5a1c42cdb4053c0
zwc1986 <zwc1986@9c348496-b637-11de-9846-1b058f2a8eb1>
2009-10-26 11:47:36 +0000

    删除了项目, 准备重新建

--- trunk/gtdrom/testcase/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include ("../data/sqlhelper.php");
-
-$result  =sqlhelper::process("delete from test where ID = 10");
-echo "受影响的行数.$result"
-
-?>
--- trunk/gtdrom/testcase/zwc_test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by zwc, 这个文件是曾文超专用的
-*/
-require_once("../data/sqlhelper.php");
-echo sqlhelper::say();
-?>
