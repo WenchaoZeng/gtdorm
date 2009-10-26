954406dc80890dae2a53c4987fbc6b94a314568c
zwc1986 <zwc1986@9c348496-b637-11de-9846-1b058f2a8eb1>
2009-10-26 11:56:35 +0000

    将项目加到zend studio里

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+// 数据库配置信息
+$db_host = "localhost";
+$db_name  = "gtdorm";
+$db_user = "root";
+$db_pass = "123456";
+
+
+
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?
+/**
+ * 这个页面是网站的默认页面, 显示宿舍管理系统的说明之类的介绍内容
+*/
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+    <title>宿舍管理系统</title>
+</head>
+<body>
+	
+</body>
+</html>
